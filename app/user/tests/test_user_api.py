b78395725bda19b3cb34db9d670b2e409c063325
VaibhavdLights <vaibhavprajapati0101@gmail.com>
2023-09-18 00:13:47 +0530

    corrected lints

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -108,6 +108,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class PrivateUserApiTests(TestCase):
     """Tests API requests that require authentication."""
 
@@ -125,7 +126,7 @@ class PrivateUserApiTests(TestCase):
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data,{
+        self.assertEqual(res.data, {
             'name': self.user.name,
             'email': self.user.email,
         })
